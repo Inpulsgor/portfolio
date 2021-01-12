4fc1ccbc5e2ccae97f68cdb93ddda0543eeff921
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2021-01-12 21:27:11 +0200

    typewriter was added to home page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './scss/main.scss';
 import './services/helpers';
 
+import './services/animatedheadline/jquery 3.5.1';
+import './services/animatedheadline/animatedheadline';
+
 import './components/header/header';
 import './components/home/home';
 import './components/section/section';
