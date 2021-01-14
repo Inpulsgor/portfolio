5d0251016f6d38e55aa337468918c8e0e19da3dc
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2021-01-14 16:43:42 +0200

    skills section ready / animated skillbar added

--- src/services/refs.js
+++ src/services/refs.js
@@ -2,4 +2,5 @@ export default {
   header: document.querySelector('.js-header'),
   home: document.querySelector('.js-home'),
   about: document.querySelector('.js-about'),
+  abilities: document.querySelector('.js-abilities'),
 };
