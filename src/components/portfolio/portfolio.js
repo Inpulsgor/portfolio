af8ad20f75cdd515ad134d133f3b30ae5c85c599
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2021-01-15 15:48:38 +0200

    portfolio section / grid gallery in progress

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,2 +1,14 @@
+import Masonry from 'masonry-layout';
 import refs from '../../services/refs';
 import './portfolio.scss';
+
+window.onload = () => {
+  const grid = document.querySelector('.grid');
+
+  const masonry = new Masonry(grid, {
+    // itemSelector: 'grid-item',
+    gutter: 10,
+    originLeft: false,
+    originTop: false,
+  });
+};
