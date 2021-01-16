2190f77ac1955a56131776c2cd6cb76c00848904
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2021-01-16 23:10:17 +0200

    grid layout ready

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,16 +1,16 @@
-import Masonry from 'masonry-layout';
+// import Masonry from 'masonry-layout';
 import refs from '../../services/refs';
 import './portfolio.scss';
 
-window.onload = () => {
-  const grid = document.querySelector('.grid');
+// window.onload = () => {
+//   const grid = document.querySelector('.grid');
 
-  const masonry = new Masonry(grid, {
-    itemSelector: '.grid-item',
-    gutter: 10,
-    percentPosition: true,
-    resize: false,
-    // originLeft: false,
-    // originTop: false,
-  });
-};
+//   const masonry = new Masonry(grid, {
+//     itemSelector: '.grid-item',
+//     gutter: 10,
+//     percentPosition: true,
+//     // resize: false,
+//     // originLeft: false,
+//     // originTop: false,
+//   });
+// };
